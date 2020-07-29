bf3d3c3d5c3e6439e235738a2ad91b3ebf093a79
Z3R0 <www.1245731267@qq.com>
2020-07-29 09:33:57 +0800

    Create Ex1_1.java

---
+++ algorithms4/ch01/1_1/Ex1_1.java
@@ -0,0 +1,12 @@
+import edu.princeton.cs.algs4.StdOut;
+
+public class Ex1_1 {
+    public static void main(String[] args) {
+        /**
+         * 1.1.1给出以下表达式的值
+         * **/
+        StdOut.println((0 + 15) / 2);
+        StdOut.println(2.0e-6 * 100000000.1);
+        StdOut.println(true && false || true && true);
+    }
+}
