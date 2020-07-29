1b092b4a40bb2b740e83be18d1e90febf8ea6b42
Z3R0 <www.1245731267@qq.com>
2020-07-29 10:45:16 +0800

    练习1.1.7a

---
+++ algorithms4/ch01/1_1/Ex1_1_7_a.java
@@ -0,0 +1,15 @@
+import edu.princeton.cs.algs4.StdOut;
+
+public class Ex1_1 {
+    public static void main(String[] args) {
+        /**
+         * 1.1.7分别给出以下代码段打印出的值
+         * **/
+         
+        //1.1.7 a
+        double t = 9.0;
+        while (Math.abs(t - 9.0 / t) > .001)
+             t = (9.0 / t + t) / 2.0;
+        StdOut.printf("%.5f\n", t);//3.00009
+    }
+}
