f1b79201f877e45809507d5ae78a2ad9206d5a59
Z3R0 <www.1245731267@qq.com>
2020-07-29 11:05:16 +0800

    练习1.1.9
    编写一段代码，将一个正整数 N 用二进制表示并转换为一个 String 类型的值 s

---
+++ algorithms4/ch01/1_1/Ex1_1_9.java
@@ -0,0 +1,22 @@
+import edu.princeton.cs.algs4.StdOut;
+
+public class Ex1_1 {
+    public static void main(String[] args) {
+        /**
+         *1.1.9 编写一段代码，将一个正整数 N 用二进制表示并转换为一个 String 类型的值 s
+         * **/
+        //从命令行获取一个整数
+        int i = Integer.parseInt(args[0]);
+        String s = toBinaryString(i);
+        StdOut.printf("%d转换为二进制为：", i);
+        StdOut.println(s);
+    }
+
+    public static String toBinaryString(int N) {
+        String s = "";
+        for (int n = N; n > 0; n /= 2) {
+            s = (n % 2) + s;
+        }
+        return s;
+    }
+}
