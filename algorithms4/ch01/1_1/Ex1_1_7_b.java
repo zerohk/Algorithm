86806eafccfc5810a611633034874872fbd4b08b
Z3R0 <www.1245731267@qq.com>
2020-07-29 10:47:24 +0800

    练习1.1.7b

---
+++ algorithms4/ch01/1_1/Ex1_1_7_b.java
@@ -0,0 +1,14 @@
+import edu.princeton.cs.algs4.StdOut;
+
+public class Ex1_1 {
+    public static void main(String[] args) {
+        // 1.1.7 b
+        int sum = 0;
+        for (int i = 0; i < 1000; i++) {
+            for (int j = 0; j < i; j++) {
+                sum++;
+            }
+        }
+        StdOut.println(sum);//499500
+     }
+}
