b4f97e74d688e5a00394583d10fd446ffadc5062
Z3R0 <www.1245731267@qq.com>
2020-07-29 10:53:55 +0800

    练习1.1.8
    下列语句会打印出什么结果？给出解释。

---
+++ algorithms4/ch01/1_1/Ex1_1_8.java
@@ -0,0 +1,10 @@
+public class Ex1_1_8 {
+    public static void main(String[] args) {
+        /**
+         * 1.1.8下列语句会打印出什么结果？给出解释。
+         * **/
+        System.out.println('b');//b
+        System.out.println('b' + 'c');//197
+        System.out.println((char) ('a' + 4));//e
+    }
+}
