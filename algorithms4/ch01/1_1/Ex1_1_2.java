9a04da63dd32a4ca93eceeef38766e3e05b05223
Z3R0 <www.1245731267@qq.com>
2020-07-29 10:33:26 +0800

    练习1.1.2
    给出以下表达式的类型和值：
    a. (1 + 2.236)/2
    b. 1 + 2 + 3 + 4.0
    c. 4.1 >= 4
    d. 1 + 2 + "3"

---
+++ algorithms4/ch01/1_1/Ex1_1_2.java
@@ -0,0 +1,13 @@
+import edu.princeton.cs.algs4.StdOut;
+
+public class Ex1_1 {
+    public static void main(String[] args) {
+       /**
+        * 1.1.2给出以下表达式的类型和值
+        * **/
+        StdOut.println((1 + 2.236) / 2);//double,1.618
+        StdOut.println(1 + 2 + 3 + 4.0);//double,10.0
+        StdOut.println(4.1 >= 4);//boolean,true
+        StdOut.println(1 + 2 + "3");//String,33
+    }
+}
