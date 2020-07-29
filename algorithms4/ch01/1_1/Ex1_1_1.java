9d47a929a4ab0aa8bacb8cc68636309713a5161c
Z3R0 <www.1245731267@qq.com>
2020-07-29 10:30:35 +0800

    练习1.1.1
    给出以下表达式的值：
    a. ( 0 + 15 ) / 2
    b. 2.0e-6 * 100000000.1
    c. true && false || true && true

--- algorithms4/ch01/1_1/Ex1_1_1.java
+++ algorithms4/ch01/1_1/Ex1_1_1.java
@@ -5,8 +5,7 @@ public class Ex1_1 {
         /**
          * 1.1.1给出以下表达式的值
          * **/
-        StdOut.println((0 + 15) / 2);
-        StdOut.println(2.0e-6 * 100000000.1);
-        StdOut.println(true && false || true && true);
-    }
+        StdOut.println((0 + 15) / 2);//7
+        StdOut.println(2.0e-6 * 100000000.1);//200.0000002
+        StdOut.println(true && false || true && true);//true
 }
