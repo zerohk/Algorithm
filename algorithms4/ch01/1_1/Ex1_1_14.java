8774c6b5d2e63a1f0b11077806e5d82aae5c8be1
Z3R0 <www.1245731267@qq.com>
2020-07-29 18:18:24 +0800

    练习14

---
+++ algorithms4/ch01/1_1/Ex1_1_14.java
@@ -0,0 +1,38 @@
+import edu.princeton.cs.algs4.StdOut;
+
+public class Ex1_1_14 {
+    public static void main(String[] args) {
+        /**
+         *1.1.14 编写一个静态方法 lg()，接受一个整型参数 N，返回不大于 log2N 的最大整数。 
+         * 不要使用 Math 库。
+         * **/
+        StdOut.println(lg(2));
+        StdOut.println(lg(3));
+        StdOut.println(lg(4));
+        StdOut.println(lg(5));
+        StdOut.println(lg(16));
+        StdOut.println(lg(1024));
+        StdOut.println(lg(8888));
+    }
+
+    public static int lg(int N) {
+        if (N == 0) return -1;
+        if (N == 1) return 0;
+        int i = 2;
+        int cnt = 1;
+        while (i * 2 <= N) {
+            cnt++;
+            i = i * 2;
+        }
+        return cnt;
+    }
+}
+/**
+ * 1
+ * 1
+ * 2
+ * 2
+ * 4
+ * 10
+ * 13
+ * **/
