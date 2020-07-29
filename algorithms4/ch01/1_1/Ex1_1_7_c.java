0dffd642f2043cf5968d37fc574802ad5eed4f35
Z3R0 <www.1245731267@qq.com>
2020-07-29 10:48:45 +0800

    练习1_1_7c

---
+++ algorithms4/ch01/1_1/Ex1_1_7_c.java
@@ -0,0 +1,14 @@
+import edu.princeton.cs.algs4.StdOut;
+
+public class Ex1_1 {
+    public static void main(String[] args) {
+        //1.1.7 c
+        int sum = 0;
+        for (int i = 1; i < 1000; i *= 2) {
+            for (int j = 0; j < 1000; j++) {
+                sum++;
+            }
+        }
+        StdOut.println(sum);//10000
+     }
+}
