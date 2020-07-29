50a04ca1f60366294a13f961f93ceadac3476834
Z3R0 <www.1245731267@qq.com>
2020-07-29 11:30:28 +0800

    Create Ex1_1_12.java

---
+++ algorithms4/ch01/1_1/Ex1_1_12.java
@@ -0,0 +1,26 @@
+public class Ex1_1_12 {
+    public static void main(String[] args) {
+        /**
+         *1.1.12 以下代码段会打印出什么结果？
+         * **/
+        int[] a = new int[10];
+        for (int i = 0; i < 10; i++)
+            a[i] = 9 - i;
+        for (int i = 0; i < 10; i++)
+            a[i] = a[a[i]];
+        for (int i = 0; i < 10; i++)
+            System.out.println(i);
+    }
+}
+/**
+ * 0
+ * 1
+ * 2
+ * 3
+ * 4
+ * 5
+ * 6
+ * 7
+ * 8
+ * 9
+ * **/
